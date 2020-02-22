5db3c9dec033ea383f7a1d3b765aa9adb2d2c2a0
RyoKoibuchi <ryouhb0504@icloud.com>
2020-02-22 15:51:55 +0900

    make_model

--- DjangoApp1/settings.py
+++ DjangoApp1/settings.py
@@ -119,8 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+AUTH_USER_MODEL = 'board.User'
+
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-MEDIA_URL = '/media_board/'
+MEDIA_URL = '/media/'
--- DjangoApp1/urls.py
+++ DjangoApp1/urls.py
@@ -17,8 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from django.conf import settings
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('DjangoApp1/board/', include('board.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
