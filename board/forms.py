da2b706e932fc43d7ce41246f2c958ad629793fe
RyoKoibuchi <ryouhb0504@icloud.com>
2020-03-01 22:58:34 +0900

    change_ui

--- board/forms.py
+++ board/forms.py
@@ -37,9 +37,19 @@ class UserForm(forms.ModelForm):
         model = User
         fields = ("name", "email", "picture")
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+
 
 class ThreadForm(forms.ModelForm):
 
     class Meta:
         model = Thread
         fields = ("title",)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
